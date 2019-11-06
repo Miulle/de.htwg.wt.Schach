2158f4c56c4377167f3d7570171d5d7ebdb10031
Miulle <mika.strandb@gmail.com>
2019-11-06 16:33:52 +0100

    implement highlighting and first interation of JS.

--- public/javascripts/chess.js
+++ public/javascripts/chess.js
@@ -4,4 +4,19 @@ function registerClick(){
 
 $( document ).ready(function() {
     console.log( "Document is ready" );
+    registerClick();
 });
+
+function highlightTest() {
+    alert('clicked!');
+}
+
+function clickFct(elmnt) {
+    elmnt.style.color = "red";
+}
+
+function registerClick() {
+
+    $("TEST").click(function() {highlightTest()});
+
+}
