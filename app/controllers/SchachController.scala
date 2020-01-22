0ca8b846d218aa51b18b9a02f29be15c7d17c1c3
nTz <cristian_ntz@yahoo.com>
2020-01-22 01:24:58 +0100

    first Auth commit

--- app/controllers/SchachController.scala
+++ app/controllers/SchachController.scala
@@ -9,12 +9,33 @@ import akka.actor.ActorSystem
 import akka.stream.Materializer
 import akka.actor._
 import de.htwg.se.ChinaSchach.controller.controllerComponent.Changed
+import com.mohiva.play.silhouette.api.actions.SecuredRequest
+import com.mohiva.play.silhouette.api.{ HandlerResult, Silhouette }
+import com.mohiva.play.silhouette.api.repositories.AuthInfoRepository
+import com.mohiva.play.silhouette.impl.providers.GoogleTotpInfo
+import org.webjars.play.WebJarsUtil
+import play.api.i18n.I18nSupport
+import utils.auth.DefaultEnv
+
+import models.User
+
 
 import scala.concurrent.{ ExecutionContext, Future }
 import scala.swing.Reactor
 
 @Singleton
-class SchachController @Inject()(cc: ControllerComponents) (implicit system: ActorSystem, mat: Materializer, ec: ExecutionContext) extends AbstractController(cc) {
+class SchachController @Inject()(
+                                  components: ControllerComponents,
+                                  silhouette: Silhouette[DefaultEnv],
+                                  authInfoRepository: AuthInfoRepository
+                                )(
+                                  implicit
+                                  webJarsUtil: WebJarsUtil,
+                                  assets: AssetsFinder,
+                                  system: ActorSystem,
+                                  mat: Materializer,
+                                  ec: ExecutionContext
+                                ) extends AbstractController(components) with I18nSupport {
   val gameController = ChinaSchach.controller
   def boardAsText = gameController.drawGameboard()
 
@@ -22,24 +43,31 @@ class SchachController @Inject()(cc: ControllerComponents) (implicit system: Act
     Ok(views.html.index())
   }
 
-  def schach = Action {
-    Ok(views.html.schach(gameController))
+  def schach = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
+    authInfoRepository.find[GoogleTotpInfo](request.identity.loginInfo).map { totpInfoOpt =>
+      Ok(views.html.schach(gameController, request.identity, totpInfoOpt))
+    }
   }
 
-  def move (from1: Int, from2: Int, to1: Int, to2: Int) = Action {
-    gameController.savePiecePoint(Point(from1, from2))
-    gameController.getSelectedPoint(Point(to1, to2))
-    Ok(gameController.gameToJson())
+  def move (from1: Int, from2: Int, to1: Int, to2: Int) = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
+    authInfoRepository.find[GoogleTotpInfo](request.identity.loginInfo).map { totpInfoOpt =>
+      gameController.savePiecePoint(Point(from1, from2))
+      gameController.getSelectedPoint(Point(to1, to2))
+      Ok(gameController.gameToJson())
+    }
   }
 
-  def restart = Action.async {
-    gameController.reset()
-    //Ok(views.html.schach(gameController))
-    Future(Ok(views.html.chessVue(gameController)))
+  def restart = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
+    authInfoRepository.find[GoogleTotpInfo](request.identity.loginInfo).map { totpInfoOpt =>
+      gameController.reset()
+      Future(Ok(views.html.chessVue(gameController)))
+    }
   }
 
-  def gameToJson = Action {
-    Ok(gameController.gameToJson())
+  def gameToJson = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
+    authInfoRepository.find[GoogleTotpInfo](request.identity.loginInfo).map { totpInfoOpt =>
+      Ok(gameController.gameToJson())
+    }
   }
 
   def socket = WebSocket.accept[String, String] { request =>
@@ -53,13 +81,14 @@ class SchachController @Inject()(cc: ControllerComponents) (implicit system: Act
     Ok(views.html.test())
   }
 
-  def offlineChess = Action {
-    implicit request: Request[AnyContent] =>
+  def offlineChess = silhouette.UnsecuredAction.async { implicit request: Request[AnyContent] =>
       Ok(views.html.offlineChess())
   }
 
-  def chessVue = Action.async {
-    Future(Ok(views.html.chessVue(gameController)))
+  def chessVue = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
+    authInfoRepository.find[GoogleTotpInfo](request.identity.loginInfo).map { totpInfoOpt =>
+      Ok(views.html.chessVue(gameController, request.identity, totpInfoOpt))
+    }
   }
 
   object SchachWebSocketActorFactory {
